cefdaeb0fdafcb65500065370cd75b70f1e3ce23
alexanderchainsaw <alexanderkolesov.contact@gmail.com>
2023-07-21 16:30:04 +0300

    added magenta

--- class_paint.py
+++ class_paint.py
@@ -2,31 +2,36 @@ from colorama import Fore, Style
 
 
 class Paint:
-    """Paint class for painting string/fstring objects with different colors."""
+    """Paint class for painting string/fstring objects with different colors"""
 
     def __init__(self, strng):
-        """String object needed for initialization."""
+        """String object needed for initialization"""
         self.strng = strng
 
         self._red = Fore.RED
         self._green = Fore.GREEN
         self._yellow = Fore.LIGHTYELLOW_EX
         self._cyan = Fore.LIGHTCYAN_EX
+        self._magenta = Fore.MAGENTA
 
         self.reset = Style.RESET_ALL
 
     def red(self):
-        """Paint object red."""
+        """Paint object red"""
         return f"{self._red}{self.strng}{self.reset}"
 
     def green(self):
-        """Paint object green."""
+        """Paint object green"""
         return f"{self._green}{self.strng}{self.reset}"
 
     def yellow(self):
-        """Paint object yellow."""
+        """Paint object yellow"""
         return f"{self._yellow}{self.strng}{self.reset}"
 
     def cyan(self):
-        """Paint object cyan."""
+        """Paint object cyan"""
         return f"{self._cyan}{self.strng}{self.reset}"
+
+    def magenta(self):
+        """Paint object magenta"""
+        return f"{self._magenta}{self.strng}{self.reset}"
