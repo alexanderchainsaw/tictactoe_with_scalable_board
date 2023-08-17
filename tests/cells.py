0ec92fc6c92f7daf0a487a5f0086a4acb8d5dbc0
alexanderchainsaw <alexanderkolesov.contact@gmail.com>
2023-08-18 00:03:01 +0300

    add exec time test file
    separate exec time tests into separate file

--- tests/cells.py
+++
@@ -1,3 +0,0 @@
-size3 = [i for i in range(9)]
-size10 = [i for i in range(100)]
-size100 = [i for i in range(1000)]
--- tests/test_tictactoe.py
+++ tests/test_tictactoe.py
@@ -1,24 +1,16 @@
 import unittest
-import timeout_decorator
+
 from tictactoe_with_scalable_board.class_tictactoe import TicTacToe
-import cells
+
 
 a = TicTacToe()
 
 
 class TestTicTacToe(unittest.TestCase):
     """Testing TicTacToe class' functionality and efficiency"""
-    @timeout_decorator.timeout(5)
-    def test_win_5sec_size3(self):
-        a.cells = cells.size3
-
-    @timeout_decorator.timeout(5)
-    def test_win_5sec_size10(self):
-        a.cells = cells.size10
-
-    @timeout_decorator.timeout(5)
-    def test_win_5sec_size10(self):
-        a.cells = cells.size100
+    pass
 
 
+if __name__ == '__main__':
+    unittest.main()
 
