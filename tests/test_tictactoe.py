19c99b7bc4d954a10384fc6ba1b23b08c29ad651
alexanderchainsaw <alexanderkolesov.contact@gmail.com>
2023-08-17 17:00:47 +0300

    create tests
    create directory for testing-related files

---
+++ tests/test_tictactoe.py
@@ -0,0 +1,10 @@
+import unittest
+from tictactoe_with_scalable_board.class_tictactoe import TicTacToe
+
+a = TicTacToe
+
+
+class TestTicTacToe(unittest.TestCase):
+
+    def test_win(self):
+        pass
