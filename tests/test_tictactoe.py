d15b57383b5694ce897b2c28950b67dc55a6b75a
alexanderchainsaw <alexanderkolesov.contact@gmail.com>
2023-08-17 17:11:19 +0300

    prepare for writing tests

--- tests/test_tictactoe.py
+++ tests/test_tictactoe.py
@@ -5,6 +5,9 @@ a = TicTacToe
 
 
 class TestTicTacToe(unittest.TestCase):
-
+    """Testing the main usage cycle of the TicTacToe class:
+    collect or reject proper size
+    generate proper cells and rows
+    assert proper amount of wins at different sized boards"""
     def test_win(self):
         pass
